c648c889db90f688d426770f0d212ce597370778
elxan2112 <63899812+elxan2112@users.noreply.github.com>
2020-10-23 20:33:28 +0400

    Update Program.cs

--- Zadanie1/Zadanie1/Program.cs
+++ Zadanie1/Zadanie1/Program.cs
@@ -19,6 +19,8 @@ namespace Zadanie1
             num1.GetPoint();
             num1.GetDistance();
             num1.GetQuarter();
+            Console.WriteLine("Для продолжения нажмите любую клавишу!");
+                            Console.ReadKey();
 
         }
 
